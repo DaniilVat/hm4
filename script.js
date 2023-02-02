b2f1d7b1de87e886aab2c042c834b20598dc31ff
DaniilVat <malouw211@gmail.com>
2023-02-02 16:14:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+// Задача 1
+
+//let arr = [1, 2, 3, 4, 5];
+
+//for (let i = 0; i < arr.length; i++ ) {
+ //   document.write(arr[i]);
+//}
+
+// Задача 2
+
+//let arr = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7]
+
+//for(i = 0; i < arr.length; i++) {
+//   if (arr[i] < -3 && arr[i] > -10){
+//    console.log(arr[i])}
+//}     
+
+// Задача 3
+
+//let arr = [];
+//let result = 0 ;
+//for( let i = 27;  i < 57; i++)
+//{
+//   arr.push(i);
+    
+    
+//}
+//console.log(arr);
+
+//for(let i = 0; i < arr.length; i++ )  {
+//    result += arr[i];
+//}
+
+//console.log(result);
+
+// Задача 4
+
+//let arr = ['10', '20', '30', '50', '235', '3000']
+
+//for (let i = 0; i < arr.length; i++) {
+
+   // if (String(arr[i])[0] == '1' || String(arr[i])[0] //== '2' || String(arr[i])[0] == '5') {
+//
+   //     console.log(arr[i]);
+//} 
+//}
+
+
+// Задача 5 
+//let arr = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
+//for (let i = 0; i < arr.length; i++) {
+ // if (i > 4) {
+ //   document.write('<b>' + arr[i] + '</b>' + ' ');
+ // } else {
+ // document.write (arr[i]+ '  ')
+ // }
+//}
+
+
+// Задача 6
+
+//let arr = ['ап', 3, 4, 5,]
+
+//arr.push(6);
+
+//console.log (arr[arr.length-1])
+
+//
+
+
+
+
+
+
+
+
+
+
